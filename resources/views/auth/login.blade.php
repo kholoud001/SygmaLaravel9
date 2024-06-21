1f154fd4366cd308093152e95a2d78ef7b8694e7
kholoud001 <kholoud.sanak@gmail.com>
2024-06-21 13:08:14 +0100

    auth/login blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,37 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sygma.AI</title>
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+<body class="h-screen w-full" style="background-image: url('{{ asset('images/login.png') }}'); background-size: cover;">
+    <div class="h-full flex justify-end mx-12 items-center">
+        <div class="h-max w-72">
+            <div class="bg-[#008080] bg-opacity-75 rounded-lg">
+                <div class="flex flex-col h-[402px] items-center justify-between px-4 py-2">
+                    <div class="flex flex-col items-center">
+                        <img src="{{ asset('images/SygmaLogo2.png') }}" alt="Sygma Logo">
+                        <form method="POST" action="{{ route('login') }}" class="bg-[#00A1A1] p-4 flex flex-col gap-2 rounded-lg">
+                            @csrf
+                            <input class="z-50 outline-none w-full rounded-lg p-2" type="text" name="email" placeholder="Identifiant...">
+                            <input class="z-50 outline-none w-full rounded-lg p-2" type="password" name="password" placeholder="Mot de passe...">
+                            <button type="submit" class="mt-2 bg-transparent underline text-white text-sm">Mot de passe oublié?</button>
+                        </form>
+                    </div>
+                    <a href="{{ route('dashboard') }}" class="z-50 mb-4 font-bold bg-[#00A1A1] px-8 py-2 text-white rounded-lg text-xl">Se connecter</a>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+    </div>
+    <div class="flex justify-end items-end h-full w-full fixed top-0 z-5">
+        <div class="w-full h-fit bg-[#008080]">
+            <div class="flex justify-between px-4 py-2 items-center">
+                <p class="text-md text-white">© Copyright SYGMA.AI 2024</p>
+                <img src="{{ asset('images/SygmaLogo.png') }}" class="w-1/12" alt="Sygma Logo">
+            </div>
         </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</body>
+</html>
